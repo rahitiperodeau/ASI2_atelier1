e31373fb1603cf47edc1ccef4a6070cf86efd34b
seraphinandrieux <seraphin.andrieux@cpe.fr>
2019-10-04 12:16:14 +0200

    Created shared component Card and ListItem. Card doesn't contain a button

---
+++ React1/Shared_Component/Card/containers/Visual.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+class Visual extends Component {
+    constructor(props) {
+        super(props);        
+    }
+  
+  render() {
+      let render_visual;
+      switch(this.props.type){
+        case "img":
+            render_visual=(
+                <img 
+                    className='imgCard' 
+                    src={this.props.src}  
+                />
+                );
+        break;
+        case "video":
+              render_visual=(
+              <object  width="100%" height="100%"
+                        data={this.props.src}>
+                </object>
+                );
+        break;
+              
+        }
+      
+      
+    return (
+            <div className="thumbnail">
+                
+                <div class="blurring dimmable image">
+                    
+                    <div class="ui fluid image">
+                        
+                    {render_visual}
+                    </div>
+                </div>
+        </div>
+           
+    );
+  }
+}
+
+export default Visual;
