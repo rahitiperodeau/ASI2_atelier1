93f6299ea7bc122f5e400446015d2d5050bd7b6c
Rahiti Perodeau <rahiti.perodeau@cpe.fr>
2019-10-04 08:41:18 +0200

    adding teacher's monilitc project

---
+++ asi2-backendmarket-monolithic-student/src/main/java/com/cpe/springboot/store/controller/StoreRestController.java
@@ -0,0 +1,29 @@
+package com.cpe.springboot.store.controller;
+
+import com.cpe.springboot.store.model.StoreOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+//ONLY FOR TEST NEED ALSO TO ALLOW CROOS ORIGIN ON WEB BROWSER SIDE
+@CrossOrigin
+@RestController
+public class StoreRestController {
+
+	
+
+	@Autowired
+	private StoreService storeService;
+	
+	@RequestMapping(method= RequestMethod.POST,value="/buy")
+	private boolean getAllCards(@RequestBody StoreOrder order) {
+		return storeService.buyCard(order.getUser_id(),order.getCard_id());
+
+	}
+	
+	@RequestMapping(method=RequestMethod.POST,value="/sell")
+	private boolean getCard(@RequestBody StoreOrder order) {
+		 return storeService.sellCard(order.getUser_id(),order.getCard_id());
+	}
+	
+}
